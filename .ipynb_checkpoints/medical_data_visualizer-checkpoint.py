20c1b312a14ed1fe1bcad52cad31d3caddc4b32d
jeremyhatcher <113803413+jeremyhatcher@users.noreply.github.com>
2025-03-16 12:47:10 -0700

    Completed Project

--- .ipynb_checkpoints/medical_data_visualizer-checkpoint.py
+++ .ipynb_checkpoints/medical_data_visualizer-checkpoint.py
@@ -21,16 +21,20 @@ def draw_cat_plot():
     df_cat = pd.melt(df, id_vars= ['cardio'], value_vars= ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
     
     # 6
-    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='count')
+    print(df_cat)
     
+
     # 7
     catplot = sns.catplot(
-        x= 'variable', y= 'total', hue= 'value', col= 'cardio',
-        data= df_cat, kind= 'bar', height= 5, aspect= 1
+        x='variable', y='count', hue='value', col='cardio',
+        data= df_cat, kind='bar', height= 5, aspect= 1
     )
+
 
     # 8
     fig = catplot.fig
+
 
     # 9
     fig.savefig('catplot.png')
@@ -39,29 +43,22 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = df[
-        (df['ap_lo'] <= df['ap_hi']) & 
-        (df['height'] >= df['height'].quantile(0.025)) & 
-        (df['height'] <= df['height'].quantile(.975)) & 
-        (df['weight'] >= df['weight'].quantile(0.025)) & 
-        (df['weight'] <= df['weight'].quantile(.975))
-    ]
+    df_heat = None
 
     # 12
-    corr = df_heat.corr()
+    corr = None
 
     # 13
-    mask = np.triu(np.ones_like(corr, dtype=bool))
+    mask = None
+
+
 
     # 14
-    fig, ax = plt.subplots(figsize=(10, 8))
+    fig, ax = None
 
     # 15
-    sns.heatmap(
-        corr, mask=mask, annot=True, fmt=".1f",
-        vmax=.3, vmin=-.16, center=.08, square=True, linewidths=0.5,
-        cbar_kws={"shrink": 0.75}
-    )
+
+
 
     # 16
     fig.savefig('heatmap.png')
